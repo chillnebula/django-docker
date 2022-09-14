9d0048c22911015954897380aaa5954d60311298
chillnebula <113555735+chillnebula@users.noreply.github.com>
2022-09-15 00:02:29 +0200

    Create urls.py

---
+++ eventapi/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from . import views
+#from .views import   GeoPointCreate, GeoPointList ,EventList, EventCreate,EventAllCreate, EventAllList
+
+urlpatterns = [
+    
+    path('', views.Event_list,name='event'),
+    #path('', views.EventAll_list,name='event'),
+
+    #path('hello/', views.say_hello),
+    #path('Event/create/', EventCreate.as_view(),name='create-event'),
+    #path('Event/', EventList.as_view(),name = 'event-list'),
+
+    #path('GeoPoint/create/', GeoPointCreate.as_view(), name = 'geopoint-create'),
+    #path('GeoPoint/', GeoPointList.as_view(),name = 'geopoint-list'),
+
+    #path('EventAll/create/', EventAllCreate.as_view(), name= 'eventAll-create'),
+    #path('EventAll/', EventAllList.as_view(), name = 'eventAll-list'),
+]
